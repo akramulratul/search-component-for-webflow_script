912fcd8b35f6f2e89edadfcb13b64c68110d7534
Akramul Ratul <akramulratul@gmail.com>
2023-05-29 12:28:44 +0600

    background remove

--- src/index.js
+++ src/index.js
@@ -50,76 +50,74 @@ function App() {
     setCheckOutSelected(true);
   };
   return (
-    <div style={{ background: "black", height: "256px" }}>
-      <div className="search-component">
-        <div className="search-field">
-          <div className="search-details">
-            <div className="input-item first-input">
-              <label className="title-text">Location</label>
+    <div className="search-component">
+      <div className="search-field">
+        <div className="search-details">
+          <div className="input-item first-input">
+            <label className="title-text">Location</label>
+            <input
+              type="text"
+              placeholder="Location"
+              onChange={(e) => setLocation(e.target.value)}
+            />
+          </div>
+          <div className="mobileFlex">
+            <div className="input-item middle-input">
+              <label className="title-text">Check-in</label>
               <input
-                type="text"
-                placeholder="Location"
-                onChange={(e) => setLocation(e.target.value)}
+                className="inputDate hide-date-icon hidden"
+                type="date"
+                //ref={checkInDateRef}
+                ref={checkInDateRef}
+                //value={formatSelectedDate(date[0].startDate)} // Convert to string in the format "YYYY-MM-DD"
+                // value={
+                //   checkInSelected
+                //     ? date[0].startDate.toISOString().split("T")[0]
+                //     : checkOutPlaceholder
+                // } // Convert to string in the format "YYYY-MM-DD"
+
+                onChange={(e) => {
+                  const newDate = [...date];
+                  newDate[0].startDate = new Date(e.target.value);
+                  setDate(newDate);
+                }}
+                value={checkInSelected ? checkInDate : checkInPlaceholder}
+                onClick={inputDate}
               />
             </div>
-            <div className="mobileFlex">
-              <div className="input-item middle-input">
-                <label className="title-text">Check-in</label>
-                <input
-                  className="inputDate hide-date-icon hidden"
-                  type="date"
-                  //ref={checkInDateRef}
-                  ref={checkInDateRef}
-                  //value={formatSelectedDate(date[0].startDate)} // Convert to string in the format "YYYY-MM-DD"
-                  // value={
-                  //   checkInSelected
-                  //     ? date[0].startDate.toISOString().split("T")[0]
-                  //     : checkOutPlaceholder
-                  // } // Convert to string in the format "YYYY-MM-DD"
-
-                  onChange={(e) => {
-                    const newDate = [...date];
-                    newDate[0].startDate = new Date(e.target.value);
-                    setDate(newDate);
-                  }}
-                  value={checkInSelected ? checkInDate : checkInPlaceholder}
-                  onClick={inputDate}
-                />
-              </div>
-              <div className="input-item last-input">
-                <label className="title-text">Check-out</label>
-                <input
-                  className="inputDate hide-date-icon hidden"
-                  type="date"
-                  ref={checkOutDateRef}
-                  //value={formatSelectedDate(date[0].startDate)} // Convert to string in the format "YYYY-MM-DD"
-                  value={checkOutSelected ? checkOutDate : checkOutPlaceholder}
-                  onChange={(e) => {
-                    const newDate = [...date];
-                    newDate[0].endDate = new Date(e.target.value);
-                    setDate(newDate);
-                  }}
-                  onClick={checkOutInput}
-                  // onChange={checkOutChange}
-                />
-              </div>
+            <div className="input-item last-input">
+              <label className="title-text">Check-out</label>
+              <input
+                className="inputDate hide-date-icon hidden"
+                type="date"
+                ref={checkOutDateRef}
+                //value={formatSelectedDate(date[0].startDate)} // Convert to string in the format "YYYY-MM-DD"
+                value={checkOutSelected ? checkOutDate : checkOutPlaceholder}
+                onChange={(e) => {
+                  const newDate = [...date];
+                  newDate[0].endDate = new Date(e.target.value);
+                  setDate(newDate);
+                }}
+                onClick={checkOutInput}
+                // onChange={checkOutChange}
+              />
             </div>
-
-            <button onClick={handleSearch} className="btn">
-              <svg
-                width="25"
-                height="20"
-                viewBox="0 0 25 20"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M23.7983 10.7075L14.7983 19.7075C14.6107 19.8951 14.3562 20.0006 14.0908 20.0006C13.8255 20.0006 13.571 19.8951 13.3833 19.7075C13.1957 19.5199 13.0903 19.2654 13.0903 19C13.0903 18.7346 13.1957 18.4801 13.3833 18.2925L20.6771 11H1.09082C0.825604 11 0.57125 10.8947 0.383714 10.7071C0.196177 10.5196 0.0908203 10.2652 0.0908203 10C0.0908203 9.73479 0.196177 9.48044 0.383714 9.2929C0.57125 9.10536 0.825604 9.00001 1.09082 9.00001H20.6771L13.3833 1.70751C13.1957 1.51987 13.0903 1.26537 13.0903 1.00001C13.0903 0.734643 13.1957 0.480147 13.3833 0.292507C13.571 0.104866 13.8255 -0.000549316 14.0908 -0.000549316C14.3562 -0.000549316 14.6107 0.104866 14.7983 0.292507L23.7983 9.29251C23.8913 9.38538 23.9651 9.49567 24.0154 9.61707C24.0657 9.73846 24.0916 9.86859 24.0916 10C24.0916 10.1314 24.0657 10.2615 24.0154 10.3829C23.9651 10.5043 23.8913 10.6146 23.7983 10.7075Z"
-                  fill="white"
-                />
-              </svg>
-            </button>
           </div>
+
+          <button onClick={handleSearch} className="btn">
+            <svg
+              width="25"
+              height="20"
+              viewBox="0 0 25 20"
+              fill="none"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M23.7983 10.7075L14.7983 19.7075C14.6107 19.8951 14.3562 20.0006 14.0908 20.0006C13.8255 20.0006 13.571 19.8951 13.3833 19.7075C13.1957 19.5199 13.0903 19.2654 13.0903 19C13.0903 18.7346 13.1957 18.4801 13.3833 18.2925L20.6771 11H1.09082C0.825604 11 0.57125 10.8947 0.383714 10.7071C0.196177 10.5196 0.0908203 10.2652 0.0908203 10C0.0908203 9.73479 0.196177 9.48044 0.383714 9.2929C0.57125 9.10536 0.825604 9.00001 1.09082 9.00001H20.6771L13.3833 1.70751C13.1957 1.51987 13.0903 1.26537 13.0903 1.00001C13.0903 0.734643 13.1957 0.480147 13.3833 0.292507C13.571 0.104866 13.8255 -0.000549316 14.0908 -0.000549316C14.3562 -0.000549316 14.6107 0.104866 14.7983 0.292507L23.7983 9.29251C23.8913 9.38538 23.9651 9.49567 24.0154 9.61707C24.0657 9.73846 24.0916 9.86859 24.0916 10C24.0916 10.1314 24.0657 10.2615 24.0154 10.3829C23.9651 10.5043 23.8913 10.6146 23.7983 10.7075Z"
+                fill="white"
+              />
+            </svg>
+          </button>
         </div>
       </div>
     </div>
